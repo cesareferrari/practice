5c8001580aff40b96f58c7a6153512300d8abf99
Cesare Ferrari <cesare@maryandferrari.com>
2021-10-07 11:36:03 -0400

    Add configuration for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: Rails.application.credentials[:mailgun][:port],
+    address: Rails.application.credentials[:mailgun][:address],
+    domain: Rails.application.credentials[:mailgun][:domain],
+    user_name: Rails.application.credentials[:mailgun][:user_name],
+    password: Rails.application.credentials[:mailgun][:password],
+    authentication: :plain
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
